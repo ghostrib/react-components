5e472bdb2ae2490714b7ec8516239862e30223ec
ghostrib <sh4rkbot@gmail.com>
2018-11-26 15:54:31 -0600

    (feat) Add onClick event

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// TODO
+function App() {
+    return (
+        <div>
+            <h1>Shopping List:</h1>
+            <GroceryListItem list={['Cucumber', 'Kale', 'mana']} />
+        </div>
+    )
+}
+
+const GroceryListItem = (props) => {
+
+    const handleChange = (event) => {
+        console.log('event changed or some shit..')
+        event.preventDefault()
+    }
+
+    return (
+        <ul>
+            <li onClick={handleChange}>{props.list[0]}</li>
+            <li>{props.list[1]}</li>
+            <li>{props.list[2]}</li>
+        </ul>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
